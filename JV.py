5058cf0d78ab5156fc4dbfe50d60e2ba3381c140
Javier García <emigarcia2007@gmail.com>
2024-11-30 14:07:22 -0600

    Nuevo archivo de ingreso

---
+++ JV.py
@@ -0,0 +1,9 @@
+user_name = "Dave"  
+if user_name == "Dave":   
+    print("Get off my computer Dave!")   
+elif user_name == "angela_catlady_87":   
+    print("I know it is you, Dave! Go away!")   
+elif user_name == "Codecademy":   
+    print("Access Granted.")   
+else:   
+    print("Username not recognized.")  
