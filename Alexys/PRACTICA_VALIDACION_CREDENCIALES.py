c0bce43dfd6f017f68e817e73a8f56db28cbe219
ALEXYS AVENDANO MONFORT <alexysavenda110406@gmail.com>
2024-12-07 22:32:58 -0600

    AGREGUE DOS USUARIOS NUEVOS, TRATE DE REALIZAR UN REGISTRO USUARIO (NO COMPLETO),

--- Alexys/PRACTICA_VALIDACION_CREDENCIALES.py
+++ Alexys/PRACTICA_VALIDACION_CREDENCIALES.py
@@ -1,9 +1,18 @@
 class CREDENCIAL():
     def __init__(self,USER,PASSWORD):
         self.USUARIO = USER
-        self.COntrasena = PASSWORD
+        self.CONTRASENA = PASSWORD
         pass
 
+
+class VALIDACION():
+    def __init__(self):
+        self.USUARIO = []
+        pass
+
+    def  REGISTRO_USUARIO(self,CREDENCIAL):
+        self.USUARIO.append(CREDENCIAL)
+        
     def VALIDACION(self):
         user =''
         while user != self.USUARIO:
@@ -11,14 +20,16 @@ class CREDENCIAL():
             user = input ('USUARIO:')
             if user == self.USUARIO:
                 password=''
-                while password != self.COntrasena:
+                while password != self.CONTRASENA:
                     password = input ('CONTRASEÑA:')
-                    if password == self.COntrasena:
+                    if password == self.CONTRASENA:
                         print('ACCESO CONCEDIDO')
                     else:
                         print('CONTRASEÑA INCORRECTA, INTENTE DE NUEVO')
             else:
                 print('USUARIO NO EXISTENTE')
         
-USER1 = CREDENCIAL('Alexys','1234')
-USER1.VALIDACION()
+USER1 = CREDENCIAL('Alexys','123')
+USER2 = CREDENCIAL('Diego','456')
+USER3 = CREDENCIAL('Luis','789')
+
