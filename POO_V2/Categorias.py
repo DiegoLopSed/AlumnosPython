0e8d6d7d150a875abc9a4b85447b8d651d8423e6
Diego Lopez <diegolopsed160703@gmail.com>
2025-01-24 17:32:27 -0600

    Categorias

---
+++ POO_V2/Categorias.py
@@ -0,0 +1,6 @@
+"""
+Categorias de los productos existentes
+"""
+
+bebidas = ['Licores','Energeticas','Jugos Naturales','Refrescos','Hidratantes','Carbonatadas','Dieta','ZERO']
+frituras = ['Flaming Hot','Bajo Sodio','Frituras de Harina','Crujiente','Sabritas','Doritos','Ruffles']
