fcd246febdeabe41da2e4a6e77ed92a76da19c0a
Luis Eduardo Pérez Herera <F95054253@gmail.com>
2025-02-01 14:31:01 -0600

    Nueva actualización de acceso de usuarios

--- OLDDATA/Practicas_Repo/JV.py
+++ OLDDATA/Practicas_Repo/JV.py
@@ -6,7 +6,18 @@ def list_users():
     print('Lista de usuarios actual')
     for user in users:
         print(user)
-             
+
+user_name = input("Ingresa tu usuario: ")  
+if user_name == "Dave":   
+    print("Get off my computer Dave!")   
+elif user_name == "angela_catlady_87":   
+    print("I know it is you, Dave! Go away!")   
+elif user_name == "Javi":   
+    print("Access Granted.")   
+else:   
+    print("Username not recognized.")
+
+
 users = []
 op=''
 
