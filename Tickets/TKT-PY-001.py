90633bcc6902b8ea9d29147ae3e1e2601400ac80
Diego Lopez <diegolopsed160703@gmail.com>
2025-02-08 14:29:35 -0600

    Ft: TKT-PY-001 Diego-QA Correccion

--- Tickets/TKT-PY-001.py
+++ Tickets/TKT-PY-001.py
@@ -5,3 +5,22 @@ def suma():
     return res
 
 print(suma())
+
+
+"""
+1: 10 id
+2: 10 commit
+3: 10 requisitos
+4: 10 funiona
+5: 0   ticket pedido 
+
+Final : 8
+Aun que el resultado sale, el ticket pide hacer la operación
+dentro de la función 
+
+def suma(num1,num2)
+    resultado = num1+num
+    return resultado
+    
+corrige el ticket
+"""
