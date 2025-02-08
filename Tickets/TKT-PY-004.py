aae675b31e8c6933d0419194f0c31032ef58298a
Javier García <emigarcia2007@gmail.com>
2025-02-08 14:45:28 -0600

    TKT-PY-0004 estand by

---
+++ Tickets/TKT-PY-004.py
@@ -0,0 +1,13 @@
+
+for i in range(0,-0):
+    numb = 0,2,4,6,8
+    numb2 = 1,3,5,7,9
+if numb == numb:
+
+
+else: numb2 == numb2:
+    print()
+
+
+int(input('Ingresa el primer numero:'))
+int(input('Ingresa el segundo numero'))
